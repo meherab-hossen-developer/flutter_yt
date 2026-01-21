abddeb03f2c75b2b00c3b50907753674b7e9505c
Meherab Hossen <mhnnishat0102@gmail.com>
2026-01-22 01:11:17 +0600

    textfield

--- lib/currency-converter.dart
+++ lib/currency-converter.dart
@@ -5,6 +5,18 @@ class CurrencyConverter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 1.0,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(100),
+      ),
+    );
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
@@ -12,16 +24,32 @@ class CurrencyConverter extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('100',
+              Text('0',
               style: TextStyle(
-                fontSize: 200,
+                fontSize: 100,
                 color: Colors.black,
                 fontWeight: FontWeight.w700,
               ),
               ),
-              TextField(
-                style: TextStyle(
-                  color: Colors.blue
+              Padding(
+                padding: const EdgeInsets.only(left: 12.2, right: 12.2),
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Please enter the amount in BDT',
+                    hintStyle: TextStyle(
+                      color: Colors.black
+                    ),
+                    prefixIcon: Icon(Icons.monetization_on),
+                    prefixIconColor: Colors.black,
+                    filled: false,
+                    fillColor: Colors.lightBlueAccent,
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
               ),
             ],
